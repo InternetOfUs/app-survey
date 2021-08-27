4315a3f6160ac09773dd61b3f90091f9cfe4cef8
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2021-08-27 17:03:18 +0200

    Updated template in order to add the deployment feature and added the worker service. Added basic deployment playbook. Issue #7

---
+++ docker-support/run_worker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+echo "Running worker..."
+
+#
+# Important note: env variables should not be passed as arguments to the module!
+# This will allow for an easier automatisation of the docker support creation.
+#
+
+
+
+exec celery -A wenet_survey worker -B -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+
