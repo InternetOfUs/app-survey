84509bd4d20312aebf4349784b5d04c78e97f7c3
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2021-11-19 16:27:51 +0100

    Fixed bug: now the survey does not chrash when an user as an empty locale. Issue #37

--- src/authentication/tests.py
+++ src/authentication/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from unittest.mock import patch
 
-# Create your tests here.
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from wenet.model.user.profile import WeNetUserProfile
+from wenet.model.user.token import TokenDetails
+
+
+class TestHomeView(TestCase):
+
+    def test_profile_with_empty_locale(self):
+        with patch("wenet.interface.service_api.ServiceApiInterface.get_token_details") as mock_token_details:
+            with patch("wenet.interface.service_api.ServiceApiInterface.get_user_profile") as mock_get_profile:
+                settings.WENET_INSTANCE_URL = ""
+
+                mock_token_details.return_value = TokenDetails("1", "1", [])
+                mock_get_profile.return_value = WeNetUserProfile.empty("1")
+
+                client = Client()
+                session = client.session
+                session["has_logged"] = True
+                session["resource_id"] = "1"
+                session.save()
+                response = client.get("/")
+
+                self.assertEqual(200, response.status_code)
+                mock_get_profile.assert_called_once()
+                mock_token_details.assert_called_once()
