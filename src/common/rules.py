5c0812dffc7d459ba7d90a2368c4f7184c9c74e2
Stefano Tavonatti <stefano.tavonatti@u-hopper.com>
2022-09-02 15:27:50 +0200

    Handled new format of the university rules. Issue #62

--- src/common/rules.py
+++ src/common/rules.py
@@ -426,3 +426,53 @@ class UniversityMappingRule(Rule):
         else:
             logger.warning(f"Trying to apply rule but the user ID [{user_profile.profile_id}] does not match the user ID in the survey [{survey_answer.wenet_id}]")
         return user_profile
+
+
+class UniversityFromDepartmentRule(Rule):
+
+    def __init__(self, question_code: str, variable_name: str, classification: str):
+        self.question_code = question_code
+        self.variable_name = variable_name
+        self.classification = classification
+
+    @staticmethod
+    def _get_university_from_department_code(department_response: str) -> str:
+        if department_response.startswith("NUM"):
+            return "NUM"
+        elif department_response.startswith("LSE"):
+            return "LSE"
+        elif department_response.startswith("AAU"):
+            return "AAU"
+        elif department_response.startswith("UNITN"):
+            return "UNITN"
+        elif department_response.startswith("UC"):
+            return "UC"
+        else:
+            raise ValueError(f"Unable to retrieve an university from department code [{department_response}]")
+
+    def apply(self, user_profile: WeNetUserProfile, survey_answer: SurveyAnswer) -> WeNetUserProfile:
+        if self.check_wenet_id(user_profile, survey_answer):
+            if self.question_code in survey_answer.answers:
+                if isinstance(self.question_code, str) and isinstance(self.classification, str) and isinstance(self.variable_name, str) \
+                        and not isinstance(survey_answer.answers[self.question_code].answer, list) and survey_answer.answers[self.question_code].answer in self.answer_mapping:
+
+                    answer_code = survey_answer.answers[self.question_code].answer
+                    university = self._get_university_from_department_code(answer_code)
+
+                    profile_entry = {"name": self.variable_name, "classification": self.classification, "description": university, "quantity": 1}
+                    add_to_profile = True
+                    for material in user_profile.materials:
+                        if material.get("classification", "") == self.classification and material.get("name", "") == self.variable_name:
+                            material["description"] = university
+                            add_to_profile = False
+                            break
+                    if add_to_profile:
+                        user_profile.materials.append(profile_entry)
+                        logger.debug(f"updated materials with: {profile_entry}")
+            else:
+                logger.debug(f"Trying to apply rule but question code [{self.question_code}] is not selected by user")
+        else:
+            logger.warning(f"Trying to apply rule but the user ID [{user_profile.profile_id}] does not match the user ID in the survey [{survey_answer.wenet_id}]")
+        return user_profile
+
+
